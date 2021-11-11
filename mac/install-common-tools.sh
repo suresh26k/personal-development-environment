101a907c49edd235c1a853fe63559928d859181b
suresh26k <gsuresh26k@gmail.com>
2021-11-11 17:21:40 +0530

    first commit

---
+++ mac/install-common-tools.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Install brew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install iTerm2
+# brew install --cask iterm2
+
+# VSCODE Plugins
+code --install-extension ms-python.python
+code --install-extension firefox-devtools.vscode-firefox-debug
+code --install-extension 2gua.rainbow-brackets
+code --install-extension ms-vscode-remote.remote-ssh-edit
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# System Setup
+
+## MAC
+
+Go to appropriate directory
+```bash
+cd mac
+```
+
+## Common tools installation
+
+```bash
+./install-common-tools.sh
+```
