e7d1413ba8c5a10a4b091ab2f5f3585e06d87ea1
suresh26k <gsuresh26k@gmail.com>
2021-12-17 14:20:33 +0530

    Update install-common-tools.sh

--- mac/install-common-tools.sh
+++ mac/install-common-tools.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
+# Bash
+# touch ~/.bash_profile
+# echo "export PATH=${PATH}:/opt/homebrew/bin" >> .bash_profile
+
+# ZSH
+# touch ~/.zshrc
+# cat <<EOF >> ~/.zshrc
+# if [ -f ~/.bash_profile ]; then 
+#     . ~/.bash_profile;
+# fi
+# EOF
+# source ~/.zshrc
+
 # Install brew
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install iTerm2
-# brew install --cask iterm2
+brew install --cask iterm2
 
-# VSCODE Plugins
-code --install-extension ms-python.python
-code --install-extension firefox-devtools.vscode-firefox-debug
-code --install-extension 2gua.rainbow-brackets
-code --install-extension ms-vscode-remote.remote-ssh-edit
 
- # GitHub
- brew install gh
+# # VSCODE Plugins
+# code --install-extension ms-python.python
+# code --install-extension firefox-devtools.vscode-firefox-debug
+# code --install-extension 2gua.rainbow-brackets
+# code --install-extension ms-vscode-remote.remote-ssh-edit
+
+#  # GitHub
+#  brew install gh
