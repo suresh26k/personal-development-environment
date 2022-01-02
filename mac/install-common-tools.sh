9ad43e5ecd2f7417d4faa58cedc284b421e73e19
suresh26k <gsuresh26k@gmail.com>
2022-01-02 19:08:32 +0530

    updated instructions

--- mac/install-common-tools.sh
+++ mac/install-common-tools.sh
@@ -6,6 +6,7 @@
 # # Paths
 # echo "export PATH=${PATH}:/opt/homebrew/bin" >> ~/.bash_profile
 
+
 # Alias
 # echo "alias ll='ls -la'" >> ~/.bash_profile
 # #-------------
@@ -30,11 +31,88 @@
 
 
 # # VSCODE Plugins
+
+## Python
 # code --install-extension ms-python.python
+
+## Firefox Dev Tools
 # code --install-extension firefox-devtools.vscode-firefox-debug
+
+## Rainbow Brackets
 # code --install-extension 2gua.rainbow-brackets
+
+## For remote development over SSH
 # code --install-extension ms-vscode-remote.remote-ssh-edit
-code --install-extension eamodio.gitlens
+# code --install-extension ms-vscode-remote.remote-ssh
+
+## GitLens
+# code --install-extension eamodio.gitlens
+
+## 
+# code --install-extension quicktype.quicktype
+
+## Creating several projects
+# code --install-extension alefragnani.project-manager
+
+## Sourcery - For code quality
+# code --install-extension sourcery.sourcery
+
+## For Docker
+# code --install-extension ms-azuretools.vscode-docker
+
+## ToDo
+# code --install-extension gruntfuggly.todo-tree
+
+## For Boto3
+# code --install-extension boto3typed.boto3-ide
+
+## For VIM
+# code --install-extension vscodevim.vim
+
+## Mermaid Diagram
+# code --install-extension bierner.markdown-mermaid
 
 #  # GitHub
 #  brew install gh
+
+# # AWS
+# curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+# sudo installer -pkg AWSCLIV2.pkg -target /
+
+# # Oh My ZSH -------------
+
+# # Install
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# # Fonts - https://github.com/powerline/fonts
+# git clone https://github.com/powerline/fonts.git --depth=1
+# cd fonts
+# ./install.sh
+# cd ..
+# rm -rf fonts
+
+# # Theme - https://github.com/ohmyzsh/ohmyzsh/wiki/Themes#agnoster
+# sed -i ''  's/^ZSH_THEME.*$/ZSH_THEME="agnoster"/g' ~/.zshrc
+
+# # Path
+# pip3 install --user git+git://github.com/powerline/powerline
+# # After installing fonts, add fonts in appropriate app
+# # - iTerm2 => Preference -> Profile -> Text -> Font
+# # - vscode => Preference -> setting -> Font Family -> Add ('DejaVu Sans Mono for Powerline')
+
+# # Update PS1
+# # export PS1='%{%f%b%k%}$(build_prompt)'
+
+# # Install AWS CLI
+# curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+# sudo installer -pkg AWSCLIV2.pkg -target /
+
+# # Install CDK
+# sudo npm install -g aws-cdk
+
+# -----------------------
+
+
+# Vagrant
+# brew install vagrant
+# Vagrant Setup on M1 => https://gist.github.com/sbailliez/f22db6434ac84eccb6d3c8833c85ad92
--- mac/manual.md
+++ mac/manual.md
@@ -3,3 +3,32 @@
 ### Install `code` to `$PATH`
 https://code.visualstudio.com/docs/setup/mac#_launching-from-the-command-line
 
+### Settings
+
+```json
+    "python.formatting.provider": "black",          // Set python formatter as black
+    "python.formatting.blackPath": "black -S",      // Ignore quotes conversion in black
+    "window.zoomLevel": 1,                          // Zoom screen - default is zero
+    "python.analysis.typeCheckingMode": "basic",    // Other options are - basic and off 
+    "vim.smartRelativeLine": true,                  // Shows relative line numbers - helps to jump to specific lines - LineNumber + "j"
+    "editor.multiCursorModifier": "ctrlCmd",        // Helps to modify multiple lines
+    "workbench.colorCustomizations": {
+        // "tab.activeBackground": "#d79b05",       // Tab Background
+        "tab.activeBorderTop": "#ff8400",           // Tab Border Top      
+        "tab.activeBorder": "#ff8400",              // Tab Border  
+        "tab.activeForeground": "#ffcb72",          // Tab Foreground      
+        "titleBar.activeBackground": "#342a1b"      // Tab Background          
+    }
+```
+## ITERM 2
+|||
+|-|-|
+|Drop Down|https://blog.mestwin.net/drop-down-terminal-in-macos-with-iterm2/|
+
+
+## AWS
+- Installation - https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+## NodeJS
+https://nodejs.org/en/download/
+
